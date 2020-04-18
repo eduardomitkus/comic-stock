604fff3802a4bfffda8062df72d9b74248859d14
Eduardo Mitkus <eduardo.silva.mitkus@gmail.com>
2020-04-18 13:43:53 -0300

    Adiciona rota e controller para Comic

---
+++ app/Http/Controllers/ComicController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repository\ComicRepositoryInterface;
+
+class ComicController extends Controller
+{
+
+    private $comicRepository;
+
+    public function __construct(ComicRepositoryInterface $comicRepository)
+    {
+        $this->comicRepository = $comicRepository;
+    }
+    
+}
